a32ed4d0ac5279e47552b90a94ba88b512b2acef
JakkeYang <jakke.yang@gmail.com>
2016-01-26 21:01:10 +0800

    add methods

---
+++ ArduRobot.ino
@@ -0,0 +1,44 @@
+#include <Servo.h>
+
+Servo gEyes;
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
+
+void arMove()
+{
+  
+}
+
+void arStop()
+{
+  
+}
+
+void arTurnLeftBy(int aAngle)
+{
+  
+}
+
+void arTurnLeftTo(int aAngle)
+{
+  
+}
+
+void arTurnFindPath()
+{
+  
+}
+
+long arDistance()
+{
+  return 0;
+}
+
